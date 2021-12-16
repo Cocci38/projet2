fe6bfef10b6957401458596f66264f38fa85a58d
PCGameJK <j.kasperski@codeur.online>
2021-12-16 21:21:51 +0100

    +correction orthographique

--- message.php
+++ message.php
@@ -7,7 +7,7 @@ define("MSG_WARNING_LISTE_MUSIQUE_EMPTY", "<div class='m-4'>
 </div>");
 define("MSG_WARNING_MUSIC_NOT_SELECTIONNED", "<div class='m-4'>
  <div class='alert alert-warning alert-dismissible fade show'>
-<strong><center><h2>Veuillez sélectionnnez une Musique dans la liste</h2></center></strong>
+<strong><center><h2>Veuillez sélectionner une Musique dans la liste</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
 </div>");
 define("MSG_SUCCESS_ADD_MUSIC", "<div class='m-4'>
@@ -17,27 +17,27 @@ define("MSG_SUCCESS_ADD_MUSIC", "<div class='m-4'>
 </div>");
 define("MSG_SUCCESS_UP_MUSIC", "<div class='m-4'>
  <div class='alert alert-success alert-dismissible fade show'>
-<strong><center><h2>Musique Ajouté</h2></center></strong>
+<strong><center><h2>Musique mise à jour</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
 </div>");
 define("MSG_SUCCESS_DEL_MUSIC", "<div class='m-4'>
  <div class='alert alert-success alert-dismissible fade show'>
-<strong><center><h2>Musique Ajouté</h2></center></strong>
+<strong><center><h2>Musique supprimée</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
 </div>");
 define("MSG_PROBLEM_ADD_MUSIC", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
-<strong><center><h2>L'insertion à recnontrer un probème</h2></center></strong>
+<strong><center><h2>L'insertion à rencontrer un probème</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
 </div>");
 define("MSG_PROBLEM_UP_MUSIC", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
-<strong><center><h2>La mise à jour a rencontré un problème</h2></center></strong>
+<strong><center><h2>La mise à jour a rencontrée un problème</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
 </div>");
 define("MSG_PROBLEM_DEL_MUSIC", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
-<strong><center><h2>La supression à rencontrer un problème</h2></center></strong>
+<strong><center><h2>La supression à rencontrée un problème</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
 </div>");
 
