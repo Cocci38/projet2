569ba0f100a07c8345bbe8e6c8f828e8f8d16bb5
Sandra <sandra.villot038@gmail.com>
2021-12-16 14:58:26 +0100

    bouton

--- container_dashboard_list_vide.php
+++ container_dashboard_list_vide.php
@@ -1,8 +1,8 @@
 <?php
 echo "<div class=\"container py-4\">
          <div class=\"h-100 p-5 bg-light border rounded-3\">
-            <div class=\"d-grid gap-2 col-6 mx-auto\">
-               <a href=\"inserer.php\"><button type=\"button\" class=\"btn btn-outline-secondary fs-4\"><i class=\"bi bi-music-note\"></i> Ajouter <i class=\"bi bi-music-note\"></i></button></a>
+            <div class=\"d-grid gap-2 col-3\">
+               <a href=\"inserer.php\"><button type=\"button\" class=\"btn btn-outline-secondary btn-lg position-absolute top-20 start-50 translate-middle\"><i class=\"bi bi-music-note\"></i> Ajouter <i class=\"bi bi-music-note\"></i></button></a>
             </div>
          </div>
       </div>";
