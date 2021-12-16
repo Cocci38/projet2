825e918ed2622c698719c7d34760435b9c5010be
pcworkJK <j.kasperki@codeur.online>
2021-12-16 10:54:12 +0100

    correction formulaire

--- inserer.php
+++ inserer.php
@@ -19,57 +19,57 @@
     <main>
         <div class="container">
             <h4>Formulaire d'insertion de musique</h4>
+            <!--formulaire debut-->
             <form method="POST" enctype="multipart/form-data" action="add_bis.php">
+                <!-- 2 premiers champs artiste et  titre-->
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="titre">Titre</label>
+                        <label for="Titre">Titre</label>
                         <input type="text" class="form-control" id="Titre" required>
                     </div>
                     <div class="col">
-                        <label for="artiste">Artiste</label>
+                        <label for="Artiste">Artiste</label>
                         <input type="text" class="form-control" id="Artiste">
                     </div>
                 </div>
+                <!--nom de l'album et genre-->
                 <div class="row mb-3">
                     <div class="col">
                         <label for="Album">Album</label>
                         <input type="text" class="form-control" id="Album">
                     </div>
                     <div class="col">
-                        <label for="genre">Genre</label>
+                        <label for="Genre">Genre</label>
                         <input type="text" class="form-control" id="Genre">
                     </div>
                 </div>
+                <!--fichier sound-->
                 <div class="row mb-3">
+                    <div class="col">
+                        <div class="image-upload">
+                            <label for="Sound">
+                                <img src=img\cover1.png width="100px" />
+                            </label>
+                            <input id="Cover" type="file" />
+                        </div>
+                    </div>
                     <div class="col">
                         <div class="image-upload">
                             <label for="Sound">
                                 <img src=img\sound2.png width="100px" />
                             </label>
-
-                            <input id="sound" type="file" required />
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="image-upload">
-                            <label for="Cover">
-                                <img src=img\cover1.png width="100px" />
-                            </label>
-
-                            <input id="Cover" type="file" required />
+                            <input id="Sound" type="file" required />
                         </div>
                     </div>
                 </div>
         </div>
-        <!--formulaire insertion-->
-        <input id="Etat" name="Etat" type="hidden" value="add">
-        </form>
-        </div>
         <div class="container">
-            <form method="POST" enctype="multipart/form-data" action="add_bis.php">
-                <button type="submit" class="btn text-white" style="background-color: #16ade1;">Valider</button>
-                <button type="cancel" class="btn text-white" style="background-color: #1b3954;">Annuler</button>
-            </form>
+            <!--formulaire insertion-->
+            <button type="submit" class="btn text-white" style="background-color: #16ade1;">Valider</button>
+            <button type="cancel" class="btn text-white" style="background-color: #1b3954;">Annuler</button>
+            <input id="Etat" name="Etat" type="hidden" value="add">
+        </div>
+        </form>
         </div>
 
     </main>
