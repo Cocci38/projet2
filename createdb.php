776c527f74564f29f535a9b4872b45215bcc2009
Sandra <sandra.villot038@gmail.com>
2021-12-17 15:20:02 +0100

    formulaire d'inscription

--- createdb.php
+++ createdb.php
@@ -8,7 +8,7 @@ try {
     $connexiondb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-    $connexiondb->beginTransaction();
+   /* $connexiondb->beginTransaction();*/
     /*creation des champs*/
     $sql1 = "CREATE TABLE Musique(
             Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -20,8 +20,25 @@ try {
             Sound Varchar(255) NOT NULL )";
     $connexiondb->exec($sql1);
     echo 'Table Musique bien créée !<br />';
-    $connexiondb->commit();
-    $connexiondb = null;
+    /*$connexiondb->commit();
+    $connexiondb = null;*/
 } catch (PDOException $e) {
     echo "Message d'erreur : [" . $e->getMessage() . "]<br>:";
+}
+
+try {
+    //$namedb = "inscription";
+    $connexiondb = new PDO("mysql:host=$servername;dbname=$namedb", $username, $password);
+    $connexiondb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql2 = "CREATE TABLE Inscription(
+            Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(20) NOT NULL,
+            mail VARCHAR (50) NOT NULL UNIQUE,
+            `password` VARCHAR (30) NOT NULL)";
+    $connexiondb->exec($sql2);
+    echo 'Table Inscription bien créée !<br>';
+
+} catch (PDOException $e){
+    echo 'Erreur : '.$e->getMessage();
 }
