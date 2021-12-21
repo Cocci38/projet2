8ba41fccc0b92f55765a371df01a81c7324fd435
pcworkJK <j.kasperki@codeur.online>
2021-12-21 17:00:26 +0100

    test

--- tester.php
+++ tester.php
@@ -1,6 +1,33 @@
-<?php include 'add.php';
-include 'tools.php';
+<?php
+include 'config.php';
+//include 'tools.php';
+function IsDir_or_CreateIt($path)
+{
+    if (is_dir($path)) {
+        return true;
+    } else {
+        if (mkdir($path)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+$file = $_SERVER['SCRIPT_NAME'];
+$file2 = basename($_SERVER['SCRIPT_FILENAME']);
 $path = 'sound';
+echo $file;
+echo "//" . $file2;
+$valeur = substr($file, $file2);
+echo " Script_file_name-->" . basename($_SERVER['SCRIPT_FILENAME']);
+echo " Document_name-->" . $_SERVER['DOCUMENT_ROOT'];
+echo " Server_name-->" . $_SERVER['SERVER_NAME'];
+echo "Substring->" . $valeur;
+
+
+echo "<br>";
+
+
 echo IsDir_or_CreateIt($path);
 echo pathinfo("/testweb/test.txt", PATHINFO_EXTENSION);
 phpinfo();
