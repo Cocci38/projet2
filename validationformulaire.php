445af6e71ac8ffe57cea79fc648fe156e849f794
Sandra <sandra.villot038@gmail.com>
2021-12-21 16:49:14 +0100

    page de connexion

--- validationformulaire.php
+++ validationformulaire.php
@@ -1,77 +1,48 @@
 <?php
 
-function valid_donnees($donnees){
+function valid_donnees($donnees)
+{
     $donnees = trim($donnees);
     $donnees = stripslashes($donnees);
     $donnees = htmlspecialchars($donnees);
     return $donnees;
 }
 
-    $serveur = "localhost";
-    $dbname = "validationformulaire";
-    $namedb = "inscription";
-    $user = "root";
-    $pass = "";
-    $name = valid_donnees($_POST["nom"]);
-    $mail = valid_donnees($_POST["mail"]);
-    $password = valid_donnees($_POST["password"]);
+$serveur = "localhost";
+$dbname = "validationformulaire";
+$namedb = "mamusique";
+$user = "root";
+$pass = "";
+$nom = valid_donnees($_POST["nom"]);
+$mail = valid_donnees($_POST["mail"]);
+$password = hash('sha256', valid_donnees($_POST["password"]));
 
-    if (!empty($nom)
-        &&strlen($nom) <=20
-        && preg_match("^[A-Za-z '-]+$",$nom)
-        &&!empty($mail)
-        &&filter_var($mail, FILTER_VALIDATE_EMAIL)){
+if (
+    !empty($nom)
+    && strlen($nom) <= 20
+    && preg_match("/^[A-Za-z '-]+$/", $nom)
+    && !empty($mail)
+    && filter_var($mail, FILTER_VALIDATE_EMAIL)
+) {
 
     try {
-        $dbco = new PDO("mysql:host=$servername;dbname=$namedb", $username, $password);
+        $dbco = new PDO("mysql:host=$serveur;dbname=$namedb", $user, $pass);
         $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $sth = $dbco->prepare("
-        INSERT INTO form(nom, mail, password)
+        INSERT INTO inscription (name, mail, password)
         VALUES(:nom, :mail, :password)");
-        $sth->bindParam(':nom',$nom);
-        $sth->bindParam(':mail',$mail);
-        $sth->bindParam(':password',$password);
+        $sth->bindParam(':nom', $nom);
+        $sth->bindParam(':mail', $mail);
+        $sth->bindParam(':password', $password);
         $sth->execute();
 
-       // header("Location:dashboard.php");
+        header("Location:inscription.php");
 
-    } catch (PDOException $e){
-        echo 'Erreur : '.$e->getMessage();
+    } catch (PDOException $e) {
+        echo 'Erreur : ' . $e->getMessage();
     }
-}else{
-    //header("Location:inscription.php");
+} else {
+    header("Location:enregistrement.php");
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
