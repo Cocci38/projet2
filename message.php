0018aa176285dccd5ce1a0ee7625f767c54e6450
PCGameJK <j.kasperski@codeur.online>
2021-12-21 19:44:07 +0100

    correction de bug

--- message.php
+++ message.php
@@ -1,96 +1,78 @@
 <?php
-<<<<<<< HEAD
 /*--MSG LISTE--*/
 define("MSG_WARNING_LISTE_MUSIQUE_EMPTY", "<div class='m-4'>
-=======
-define("MSG_WARNING_LISTE_MUSIQUE_EMPTY", "
->>>>>>> 8ba41fccc0b92f55765a371df01a81c7324fd435
- <div class='alert alert-warning alert-dismissible fade show'>
-     <strong><center><h2>la Liste de musique est vide</h2></center></strong>
-     <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
- </div>
-");
-<<<<<<< HEAD
+<div class='alert alert-warning alert-dismissible fade show'>
+<strong><center><h2>la Liste de musique est vide</h2></center></strong>
+ <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
+ </div></div>");
 /*--MSG MUSIC--*/
 define("MSG_WARNING_MUSIC_NOT_SELECTIONNED", "<div class='m-4'>
-=======
-define("MSG_WARNING_MUSIC_NOT_SELECTIONNED", "
->>>>>>> 8ba41fccc0b92f55765a371df01a81c7324fd435
- <div class='alert alert-warning alert-dismissible fade show'>
+<div class='alert alert-warning alert-dismissible fade show'>
 <strong><center><h2>Veuillez sélectionner une Musique dans la liste</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_SUCCESS_ADD_MUSIC", "
+</div></div>");
+define("MSG_SUCCESS_ADD_MUSIC", "<div class='m-4'>
  <div class='alert alert-success alert-dismissible fade show'>
 <strong><center><h2>Musique ajoutée</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_SUCCESS_UP_MUSIC", "
+</div></div>");
+define("MSG_SUCCESS_UP_MUSIC", "<div class='m-4'>
  <div class='alert alert-success alert-dismissible fade show'>
 <strong><center><h2>Musique mise à jour</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_SUCCESS_DEL_MUSIC", "
+</div></div>");
+define("MSG_SUCCESS_DEL_MUSIC", "<div class='m-4'>
  <div class='alert alert-success alert-dismissible fade show'>
 <strong><center><h2>Musique supprimée</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_PROBLEM_ADD_MUSIC", "
+</div></div>");
+define("MSG_PROBLEM_ADD_MUSIC", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>L'insertion à rencontrer un probème</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_PROBLEM_ADD_SOUND", "
+</div></div>");
+define("MSG_PROBLEM_ADD_SOUND", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>L'insertion à rencontrer un probème</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_PROBLEM_UP_SOUND", "
+</div></div>");
+define("MSG_PROBLEM_UP_SOUND", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>La mise à jour a rencontrée un problème au niveau du fichier Audio</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_PROBLEM_UP_SOUND_ELEMENT", "
+</div></div>");
+define("MSG_PROBLEM_UP_SOUND_ELEMENT", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>Le fichier Audio n'a pas extension autorisée</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_PROBLEM_UP_IMAGE_ELEMENT", "
+</div></div>");
+define("MSG_PROBLEM_UP_IMAGE_ELEMENT",
+"<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>Le fichier Image n'est pas une extension autorisée</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-<<<<<<< HEAD
-
+</div></div>");
 define("MSG_PROBLEM_DEL_MUSIC", "<div class='m-4'>
-=======
-define("MSG_PROBLEM_DEL_MUSIC", "
->>>>>>> 8ba41fccc0b92f55765a371df01a81c7324fd435
- <div class='alert alert-danger alert-dismissible fade show'>
+<div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>La supression à rencontrée un problème</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-<<<<<<< HEAD
+</div></div>");
+
 //cover / image
 //msg probleme
 
 define("MSG_PROBLEM_UP_IMAGE", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>La mise à jour à rencontrée un problème au niveau du fichier Cover</h2></center></strong>
-=======
-define("MSG_PROBLEM_UP_IMAGE", "
- <div class='alert alert-danger alert-dismissible fade show'>
-<strong><center><h2>La mise à jour a rencontrée un problème au niveau du fichier Cover</h2></center></strong>
->>>>>>> 8ba41fccc0b92f55765a371df01a81c7324fd435
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_PROBLEM_DEL_IMAGE", "
+</div></div>");
+define("MSG_PROBLEM_DEL_IMAGE", "<div class='m-4'>
  <div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>La suppression à rencontrée un problème</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
-define("MSG_PROBLEM_ADD_IMAGE", "
+</div></div>");
+define("MSG_PROBLEM_ADD_IMAGE", "<div class='m-4'></div>
  <div class='alert alert-danger alert-dismissible fade show'>
 <strong><center><h2>L'ajout a rencontrée un problème au niveau de l'image</h2></center></strong>
 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
-</div>");
+</div></div>");
