558305ec5fa8c5473d899df5636c9c7d8c334086
Sandra <sandra.villot038@gmail.com>
2021-12-23 14:00:43 +0100

    se connecter

--- enregistrement.php
+++ enregistrement.php
@@ -12,7 +12,7 @@
     </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="CSS\style.css">
-    <title>Document</title>
+    <title>Se connecter</title>
 </head>
 
 <body class='main-bg'>
